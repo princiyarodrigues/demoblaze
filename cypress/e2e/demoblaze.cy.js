8c07e25582e1cdb6ee3a8e4092d0d2ce013c7273
princiyarodrigues <princiyarodrigues17@gmail.com>
2025-03-30 22:44:08 +0200

    Changed approach for deleting

--- cypress/e2e/demoblaze.cy.js
+++ cypress/e2e/demoblaze.cy.js
@@ -13,28 +13,29 @@ describe('Demoblaze E2E Tests', () => {
     // CREATE AN ACCOUNT
     cy.get('#navbarExample').should('be.visible'); // We need to make sure that the parent element is visible/rendered for the following actions
     cy.get('#signin2').click();
+    cy.wait(3000)
     cy.get('.modal-content').should('be.visible');
     cy.get('#sign-username').clear().type(username); //.clear is added to clear the existing data in the placeholder
     cy.get('#sign-password').clear().type(password);
     cy.get('button').contains('Sign up').click(); //using contains because of no unique locator
-    cy.on('window:alert', (str) => {
-      expect(str).to.contain('Sign up successful.');
-    });
-    cy.get('.modal-content').should('not.be.visible');
-  });
+    // cy.on('window:alert', (str) => {
+    //   expect(str).to.contain('Sign up successful.');
+    // });
+  
 
   // LOGIN
-  it('should login an account, login, add products, validate amounts, delete a product, and place an order', () => {
+  
     cy.get('#navbarExample').should('be.visible');
     cy.get('#login2').click();
     cy.get('.modal-content').should('be.visible');
-    cy.get('#loginusername').clear().type(username);
-    cy.get('#loginpassword').clear().type(password);
+    cy.wait(3000)
+    cy.get('#loginusername').clear().type(username,{ delay: 200 });
+    cy.get('#loginpassword').clear().type(password,{ delay: 200 });
     cy.get('button').contains('Log in').click();
-    cy.wait(1000);
-    cy.get('.modal-content').should('not.be.visible');
+    // cy.wait(1000);
+    // cy.get('.modal-content').should('not.be.visible');
     cy.contains('#nameofuser', username);
-
+  
     // ADD 3 PRODUCTS TO THE CART
     const productsToAdd = ['Samsung galaxy s6', 'Nokia lumia 1520', 'Nexus 6'];
     productsToAdd.forEach((product) => {  // loops all elements in array above until all 3 products are added
@@ -42,47 +43,52 @@ describe('Demoblaze E2E Tests', () => {
       cy.intercept('POST', '**/addtocart').as('addToCart');
       cy.get('.btn.btn-success.btn-lg').contains('Add to cart').click(); //3 dots in classname is because we have 3 different class and space is not a valid loactor
       cy.wait('@addToCart'); 
-      cy.on('window:alert', (str) => {
-        expect(str).to.contain('Product added.');
-      });
+      cy.wait(2000)
+      // cy.on('window:alert', (str1) => {
+      //   expect(str1).to.contain('Product added.');
+      // });
       cy.get('#nava').contains('PRODUCT STORE').click();
     });
 
- // VALIDATE INITIAL TOTAL
- cy.contains('Cart').click();
- const pricesBefore = [];
- cy.get('tr.success').each(($row) => {
-   cy.wrap($row).find('td').eq(2).invoke('text').then((text) => {
-     pricesBefore.push(parseInt(text));
-   });
- }).then(() => {
-   const initialTotal = pricesBefore.reduce((acc, val) => acc + val, 0);
-   cy.get('#totalp').should('have.text', `${initialTotal}`);
+    // VALIDATE TOTAL
+    cy.contains('Cart').click();
+    cy.wait(3000)
+    const expectedTotalAfterAdd = 1830;
+    const expectedTotalAfterDelete = 1470;
 
-   // CAPTURE FIRST ITEM PRICE
-   const deletedItemPrice = pricesBefore[0];
+    let actualTotalAfterAdd = 0;
+    let actualTotalAfterDelete = 0;
 
-   // DELETE FIRST ITEM
-   cy.intercept('POST', '**/deleteitem').as('deleteItem');
-   cy.intercept('POST', '**/viewcart**').as('viewCart');
-   cy.get('tr.success').first().contains('Delete').click();
-   cy.wait('@deleteItem');
-   cy.wait('@viewCart');
+    cy.get('tr.success').each(($row) => {
+      cy.wrap($row).find('td').eq(2).invoke('text').then((priceText) => { //invoke for exact text
+        actualTotalAfterAdd += parseInt(priceText); // initialTotal will have the value new value and keeps adding as new product value is calculated
+      });
+    }).then(() => {
+      if (expectedTotalAfterAdd === actualTotalAfterAdd){
+      cy.log('Total is correct');
+      cy.get('#totalp').should('have.text', `${expectedTotalAfterAdd}`); 
+    }
+    else {
+      cy.log('Total is incorrect');
+      expect(expectedTotalAfterAdd).to.equal(actualTotalAfterAdd); // This will fail the test case and stop the execution
+    }
+    });
 
-   // CALCULATE UPDATED TOTAL
-   const pricesAfter = [];
-   cy.get('tr.success').each(($row) => {
-     cy.wrap($row).find('td').eq(2).invoke('text').then((text) => {
-       pricesAfter.push(parseInt(text));
-     });
-   }).then(() => {
-     const updatedTotal = pricesAfter.reduce((acc, val) => acc + val, 0);
-     // Check new total
-     cy.get('#totalp').should('have.text', `${updatedTotal}`);
-     // Compare difference
-     expect(initialTotal - updatedTotal).to.eq(deletedItemPrice);
-   });
- });
+    // DELETE PRODUCT AND VALIDATE THE AMOUNT AGAIN
+    cy.intercept('POST', '**/deleteitem').as('deleteItem'); // intercept deletion
+    cy.intercept('POST', '**/viewcart**').as('viewCart');    // cart reload after deletion
+
+    cy.get('tr.success').contains('Samsung galaxy s6').closest('tr').contains('Delete').click()
+    cy.wait('@deleteItem');  // wait for delete API to complete
+    cy.wait('@viewCart');    // wait for the cart to be updated
+    cy.get('tr.success').each(($row) => {
+      cy.wrap($row).find('td').eq(2).invoke('text').then((priceText) => {
+        actualTotalAfterDelete += parseInt(priceText);
+      });
+    }).then(() => {
+      expect(actualTotalAfterDelete).to.equal(expectedTotalAfterDelete);//
+      cy.get('#totalp').should('have.text', `${expectedTotalAfterDelete}`);
+    });
 
     // PLACE THE ORDER
     cy.contains('Place Order').click();
@@ -118,7 +124,6 @@ describe('Demoblaze E2E Tests', () => {
     cy.get('.modal-content').should('not.be.visible');
     cy.contains('#nameofuser', 'princiya1')
   });
-
   // Negative Test: Invalid Credentials
   it('should not login with invalid credentials', () => {
     cy.get('#login2').click();
